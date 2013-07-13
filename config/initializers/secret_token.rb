74f02151f68ec421bf37c9aaf7ef5575c370f9f5
poc <poc@ubuntu.(none)>
2013-07-12 20:40:08 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forum::Application.config.secret_key_base = '6c735d761e99e23cf6e2f7fa3846d3468702f4807d04dbf64e5cbea38e3756bd6a2f1521ab32f4567b9c9125184e7193e8a358dfc2e7b0e231b34aed21675204'
